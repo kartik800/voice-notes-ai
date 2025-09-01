5193caca1af3105a1311bb33083bb7f816c95e59
Kartik Aggarwal <er.kartik18102016@gmail.com>
2025-09-01 22:42:47 +0530

    Update db.js

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,7 +5,7 @@ const connectToMongoDB = async () => {
     // if env don't work you can directly paste the url here
     // "mongodb://127.0.0.1:27017/voice_notes"
     await mongoose.connect(
-      process.env.MONGO_URI
+      process.env.MONGO_URI || "mongodb://127.0.0.1:27017/voice_notes"
     );
     console.log("connected to database");
   } catch (e) {
