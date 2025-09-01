b3f971b012bfec477454fb5d6d9d0c0361fbef15
Kartik Aggarwal <er.kartik18102016@gmail.com>
2025-09-02 01:54:41 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/", // backend server
+  baseURL: "https://voice-notes-ai-6x8g.vercel.app/api/", // backend server
 });
 
 export default api;
