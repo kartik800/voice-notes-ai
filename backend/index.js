5b945cd2d4416069cdf5226c536dcdce047f235e
Kartik Aggarwal <er.kartik18102016@gmail.com>
2025-09-01 23:25:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,11 +8,11 @@ import { inngest, functions } from "./src/inggest/index.js";
 import notesRouter from "./routes/notes.js";
 
 const app = express();
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://voice-notes-ai-lac.vercel.app/",
+    origin: "https://voice-notes-ai-lac.vercel.app",
     credentials: true,
   })
 );
@@ -23,16 +23,16 @@ app.use("/api/notes", notesRouter);
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-connectToMongoDB();
-// const startServer = async () => {
-//   try {
-//     await connectToMongoDB();
-//     app.listen(PORT, () => {
-//       console.log(" Server running on http://localhost:5000");
-//     });
-//   } catch (err) {
-//     console.error("Failed to start server:", err);
-//   }
-// };
+// connectToMongoDB();
+const startServer = async () => {
+  try {
+    await connectToMongoDB();
+    app.listen(PORT, () => {
+      console.log(" Server running on http://localhost:5000");
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+  }
+};
 
-// startServer();
+startServer();
