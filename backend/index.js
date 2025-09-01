9a4cb2e3bd89c5674716423bc602d264f89ef765
Kartik Aggarwal <er.kartik18102016@gmail.com>
2025-09-01 22:40:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,16 +23,16 @@ app.use("/api/notes", notesRouter);
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
-connectToMongoDB();
-// const startServer = async () => {
-//   try {
-//     await connectToMongoDB();
-//     // app.listen(PORT, () => {
-//     //   console.log(" Server running on http://localhost:5000");
-//     // });
-//   } catch (err) {
-//     console.error("Failed to start server:", err);
-//   }
-// };
+// connectToMongoDB();
+const startServer = async () => {
+  try {
+    await connectToMongoDB();
+    app.listen(PORT, () => {
+      console.log(" Server running on http://localhost:5000");
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+  }
+};
 
-// startServer();
+startServer();
