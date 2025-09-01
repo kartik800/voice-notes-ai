47d367c4ac14edf042d1abdfb1312476ada8ce8e
Kartik Aggarwal <er.kartik18102016@gmail.com>
2025-09-01 19:06:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ import { inngest, functions } from "./src/inggest/index.js";
 import notesRouter from "./routes/notes.js";
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
@@ -26,9 +26,9 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 const startServer = async () => {
   try {
     await connectToMongoDB();
-    app.listen(PORT, () => {
-      console.log(" Server running on http://localhost:5000");
-    });
+    // app.listen(PORT, () => {
+    //   console.log(" Server running on http://localhost:5000");
+    // });
   } catch (err) {
     console.error("Failed to start server:", err);
   }
